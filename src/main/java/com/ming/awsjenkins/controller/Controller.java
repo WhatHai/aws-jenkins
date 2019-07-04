0d52cb3a9946e5c3f990823dc7220eeb051069f6
whsthai <hai636@live.com>
2019-07-04 15:05:12 +0800

    Initial commit

---
+++ src/main/java/com/ming/awsjenkins/controller/Controller.java
@@ -0,0 +1,14 @@
+package com.ming.awsjenkins.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("hello2")
+public class Controller {
+    @RequestMapping("")
+    public String hello() {
+        return "helloworld2";
+
+    }
+}
---
+++ src/test/java/com/ming/awsjenkins/AwsJenkinsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ming.awsjenkins;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AwsJenkinsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
