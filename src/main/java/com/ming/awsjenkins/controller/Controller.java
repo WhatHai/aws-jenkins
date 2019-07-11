825243851803282a00153bb8e0d18a1cd4db16b4
whsthai <hai636@live.com>
2019-07-11 17:48:53 +0800

    first

--- src/main/java/com/ming/awsjenkins/controller/Controller.java
+++ src/main/java/com/ming/awsjenkins/controller/Controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class Controller {
     @RequestMapping("")
     public String hello() {
-        return "helloworld2";
+        return "helloworld1";
 
     }
 }
